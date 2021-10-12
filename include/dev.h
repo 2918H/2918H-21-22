b071ae1f762ffb5e1eba0e140e20e74a73a2d1c4
Hoverth <31601374+Hoverth@users.noreply.github.com>
2021-10-12 21:29:31 +1300

    Reversed motor directions to fit mechanism

--- include/dev.h
+++ include/dev.h
@@ -2,10 +2,10 @@
 #define _DEVICES_H_
 
 #include "api.h"
-static pros::Motor lf_mtr(9, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-static pros::Motor lb_mtr(8, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-static pros::Motor rf_mtr(3, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_ROTATIONS);
-static pros::Motor rb_mtr(2, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_ROTATIONS);
+static pros::Motor lf_mtr(9, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_ROTATIONS);
+static pros::Motor lb_mtr(8, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_ROTATIONS);
+static pros::Motor rf_mtr(3, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+static pros::Motor rb_mtr(2, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_ROTATIONS);
 
 static pros::Motor goalx_mtr(11, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_ROTATIONS);
 static pros::Motor goaly_mtr(12, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_ROTATIONS);
